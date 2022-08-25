02ba5edd8728aa4278e778ae12498166895239ac
jy0813 <busbug3@gmail.com>
2022-08-25 21:35:25 +0900

    feat: node module, npm

---
+++ 1.module/index.js
@@ -0,0 +1,6 @@
+const myvar = require("./myvar");
+const Myvar = require("./myvar");
+
+const setVar = new Myvar();
+console.log(myvar.a);
+console.log(setVar.name);
---
+++ 1.module/myvar.js
@@ -0,0 +1,8 @@
+const a = "hello a";
+function Myvar() {
+  this.name = "my instance";
+  this.hello = "hello";
+}
+
+module.exports = Myvar;
+module.exports.a = a;
