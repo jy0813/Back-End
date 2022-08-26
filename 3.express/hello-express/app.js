d04d6f31fd26427a3f2ed55a9e8a7b2e27079b1a
jy0813 <busbug3@gmail.com>
2022-08-26 17:30:15 +0900

    feat: router 설정

--- 3.express/hello-express/app.js
+++ 3.express/hello-express/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const admin = require("./routes/admin");
+const contacts = require("./routes/contacts");
 
 const app = express();
 const port = 4000;
@@ -6,6 +8,9 @@ const port = 4000;
 app.get("/", (request, response) => {
   response.send("hello express");
 });
+
+app.use("/admin", admin);
+app.use("/contacts", contacts);
 
 app.get("/jjy", (request, response) => {
   response.send("hello jjy get");
