3631fc7f08bc56d84d6ac55835dab2e6ee03bd8e
jy0813 <busbug3@gmail.com>
2022-08-26 17:49:11 +0900

    feat: nunjucks

--- 3.express/hello-express/app.js
+++ 3.express/hello-express/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
+
 const admin = require("./routes/admin");
 const contacts = require("./routes/contacts");
 
 const app = express();
 const port = 4000;
+nunjucks.configure("template", {
+  autoescape: true,
+  express: app,
+});
 
 app.get("/", (request, response) => {
   response.send("hello express");
